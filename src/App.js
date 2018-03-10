16d46c4fee4112d72c5240ec907dd416d2dad471
Gilad <freeslugs@gmail.com>
2018-03-10 16:44:23 -0500

    remove vestigal lib and configure flow -- todo fix flow errors

--- src/App.js
+++ src/App.js
@@ -1,6 +1,8 @@
+// @flow
 import React, { Component } from 'react'
 import SimpleStorageContract from '../build/contracts/SimpleStorage.json'
 import getWeb3 from './utils/getWeb3'
+// import { Button } from 'semantic-ui-react'
 
 class App extends Component {
   state = {
@@ -23,10 +25,10 @@ class App extends Component {
   }
 
   async instantiateContract() {
+    // configure and include the smart contract
     const contract = require('truffle-contract')
     const simpleStorage = contract(SimpleStorageContract)
     simpleStorage.setProvider(this.state.web3.currentProvider)
-
     // Declaring this for later so we can chain functions on SimpleStorage.
     let simpleStorageInstance
 
@@ -35,9 +37,9 @@ class App extends Component {
       const instance = await simpleStorage.deployed();
       simpleStorageInstance = instance
       // Stores a given value, 5 by default.
-      let result = await simpleStorageInstance.set(5, {from: accounts[0]})
+      // let result = await simpleStorageInstance.set(5, {from: accounts[0]})
       // Get the value from the contract to prove it worked.
-      result = await simpleStorageInstance.get.call(accounts[0])
+      const result = await simpleStorageInstance.get()
       // Update state with the result.
       this.setState({ storageValue: result.c[0] })
     })  
