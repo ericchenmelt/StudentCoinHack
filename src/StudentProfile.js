19139013d45b5eb12794784f573067960af28213
Gilad <freeslugs@gmail.com>
2018-03-11 00:57:08 -0500

    sidebar for student pages and another account fn line

---
+++ src/StudentProfile.js
@@ -0,0 +1,15 @@
+// @flow
+import React, { Component } from 'react'
+import { Container, Button, Checkbox, Form } from 'semantic-ui-react'
+
+class StudentProfile extends Component {
+  render() {
+    return (
+      <Container>
+        I'm a profile
+      </Container>
+    )
+  }
+}
+
+export default StudentProfile;
