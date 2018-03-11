0772657fdfb903072780dc3b5e7798261e3c241b
Yicen Shi <yicenshi@YicendeMBP.wireless.newschool.edu>
2018-03-11 12:56:56 -0400

    eidts

--- src/StudentWallet.js
+++ src/StudentWallet.js
@@ -1,13 +1,44 @@
 // @flow
 import React, { Component } from 'react'
-import { Container, Button, Checkbox, Form } from 'semantic-ui-react'
+import { Container, Button, Header, Checkbox, Form } from 'semantic-ui-react'
+import styled from 'styled-components';
+
+const StyledWallet = styled.div`
+
+  background: rgb(237, 248, 252);
+  background-size: cover;
+  height: 100%;
+  min-height: 100vh;
+  width:calc(100% - 150px);;
+
+
+  div.ui.text.container {
+    padding-top: 2%;
+  }
+
+
+  @media only screen and (min-width: 768px) {
+    h1.ui.header {
+      font-size: 2rem;
+      font-weight:400;
+    
+    }
+  }
+
+`;
 
 class StudentWallet extends Component {
   render() {
     return (
-      <Container>
-        Hi im a wallet
-      </Container>
+    	 <StyledWallet>
+     	  <Container>
+	        <Header as='h1'>My Wallet</Header>
+
+	    
+	      </Container>
+	      </StyledWallet>
+      
+     
     )
   }
 }
