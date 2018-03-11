ccd66963dcf586509c7eaf5cd07e97b90a92ef27
Gilad <freeslugs@gmail.com>
2018-03-10 21:41:00 -0500

    init config for hooking up backend w/ frontend, tested locally.

---
+++ contracts/Accounts.sol
@@ -0,0 +1,97 @@
+pragma solidity ^0.4.19;
+pragma experimental ABIEncoderV2;
+
+contract Accounts {
+	uint numLenders = 0;
+	uint public numStudents = 0;
+	uint public lol = 5;
+
+	struct Student {
+		string name;
+		string university;
+		address studentAccount;
+		uint minimumToRaise;
+		uint totalRaised;
+		uint idx;
+	}
+	
+	struct Lender {
+		string name;
+		address acc;
+		uint totalDonated;
+		uint balance;
+		uint idx;
+	}
+
+	mapping (address => Student) public studentMap;
+	Student[] public slist;
+	
+	mapping (address => Lender) public lenderMap;
+	Lender[] public llist;
+
+	function incrementStudent(string name) public returns(string) {
+		numStudents = numStudents + 1;
+		return name;
+	}
+
+	function addStudent(string sName, string uni, uint minRaise)
+		public {
+		var sNew = Student({
+			name: sName,
+			university: uni,
+			studentAccount: msg.sender,
+			minimumToRaise: minRaise,
+			totalRaised: 0,
+			idx: numStudents
+		});
+
+		studentMap[msg.sender] = sNew;
+		slist.push(sNew);
+
+		numStudents += 1;
+
+		// return sNew;
+
+	}
+	
+	
+	function addLender(string lName)
+		public returns (Lender){
+		var lNew = Lender({
+			name: lName,
+			acc: msg.sender,
+			balance: 0,
+			totalDonated: 0,
+			idx: numLenders
+		});
+
+		lenderMap[msg.sender] = lNew;
+		llist.push(lNew);
+
+		numLenders += 1;
+
+		return lNew;
+
+	}
+
+	//function lenderFund(uint amount) {
+
+
+
+
+	function listStudents() public returns(Student) { 
+		var sNew = Student({
+			name: "hello",
+			university: "world",
+			studentAccount: msg.sender,
+			minimumToRaise: 100,
+			totalRaised: 0,
+			idx: 0
+		});
+		slist.push(sNew);
+		return slist[0]; 
+	}
+	function getStudentCount() public view returns(uint) { return numStudents; }
+	function listLenders() public view returns(Lender[]) { return llist; }
+
+}
