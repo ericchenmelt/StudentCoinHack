96dc8c855127d7edf08eb34a5bf1cb990895731c
Gilad <freeslugs@gmail.com>
2018-03-10 19:22:35 -0500

    play around with the buttons

--- src/Landing.js
+++ src/Landing.js
@@ -1,18 +1,58 @@
 // @flow
 import React, { Component } from 'react'
 import { Link } from "react-router-dom";
-import { Button } from 'semantic-ui-react'
+import { Button, Container, Header } from 'semantic-ui-react'
+import styled from 'styled-components';
+import Grad from './images/grad.svg'
+
+const BannerSection = styled.div`
+  background: url(${Grad}) no-repeat center center;
+  background-size: cover;
+  height: 100%;
+  min-height: 100vh;
+  width: 100%;
+
+  div.ui.text.container {
+    padding-top: 10%;
+  }
+
+  .ui.green.huge.button {
+    margin-left: 20px;
+  }
+
+  @media only screen and (min-width: 768px) {
+    h1.ui.header {
+      font-size: 4rem;
+    }
+
+    h2.ui.header {
+      font-size: 2rem;
+    }
+
+    .ui.green.huge.button {
+      margin-left: 40px;
+    }
+  }
+
+`;
+
+const StyledBannerSection = () => <BannerSection>
+  <Container text>
+    <Header as='h1'>StudentCoin</Header>
+    <Header as='h2'>Revolutionizing the way students get loans</Header>
+
+    <div>
+      <Button color='purple' size='huge' as={Link} to='/funder/signup'>Fund a student</Button>
+      <Button color='green' size='huge' as={Link} to='/student/signup'>Get a loan</Button>
+    </div>
+  </Container>
+</BannerSection>
 
 class Landing extends Component {
   render() {
     return (
-      <div className="App">
-        <div>
-          <p>student coin app</p>
-
-          <Button as={Link} to='/funder/signup'>Fund a student</Button>
-          <Button as={Link} to='/student/signup'>Get a student loan</Button>
-        </div>
+      <div>
+        <StyledBannerSection />
       </div>
     );
   }
